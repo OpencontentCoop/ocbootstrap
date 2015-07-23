6a7368f4edc7657452629395e181439680707f70
Luca Realdi <luca.realdi@opencontent.it>
2015-07-23 16:40:05 +0200

    lock action before translate

--- modules/ocbtools/translate.php
+++ modules/ocbtools/translate.php
@@ -1,10 +1,20 @@
 <?php
 
+/** @var eZModule $Module */
 $Module = $Params['Module'];
 $objectId = $Params['ObjectID'];
 $http = eZHTTPTool::instance();
 
 $object = eZContentObject::fetch( $objectId );
+if ( !$object instanceof eZContentObject )
+{
+    return $Module->handleError( eZError::KERNEL_NOT_FOUND, 'kernel' );
+}
+
+if ( $http->hasGetVariable( 'force' ) )
+{
+    unLock( $object );
+}
 
 $fromLanguageCode = $http->variable( 'FromContentObjectLanguageCode', false );
 $fromLanguage = eZContentLanguage::fetchByLocale( $fromLanguageCode );
@@ -12,11 +22,28 @@ $fromLanguage = eZContentLanguage::fetchByLocale( $fromLanguageCode );
 $toLanguageCode = $http->variable( 'ToContentObjectLanguageCode', false );
 $toLanguage = eZContentLanguage::fetchByLocale( $toLanguageCode );
 
+if ( !$fromLanguage instanceof eZContentLanguage || !$toLanguage instanceof eZContentLanguage )
+{
+    return $Module->redirectTo( $object->attribute( 'main_node' )->attribute( 'url_alias' ) );
+}
+
 $versionlimit = eZContentClass::versionHistoryLimit( $object->attribute( 'contentclass_id' ) );
 $versionCount = $object->getVersionCount();
+
+if ( !$object->canEdit( false, false, false, $toLanguageCode ) )
+{
+    return $Module->handleError( eZError::KERNEL_ACCESS_DENIED, 'kernel',
+        array( 'AccessList' => $object->accessList( 'edit' ) ) );
+}
+lock( $object );
+
 if ( $versionCount < $versionlimit )
 {
-    $version = $object->createNewVersionIn( $toLanguageCode, $fromLanguageCode, false, true, eZContentObjectVersion::STATUS_INTERNAL_DRAFT );
+    $db = eZDB::instance();
+    $db->begin();
+    $version = $object->createNewVersionIn( $toLanguageCode, $fromLanguageCode, false, true, eZContentObjectVersion::STATUS_DRAFT );
+    $db->commit();
+    unLock( $object );
     return $Module->redirect( 'content', 'edit', array( $objectId, $version->attribute( 'version' ), $toLanguageCode, $fromLanguageCode ) );
 }
 else
@@ -40,18 +67,55 @@ else
         $db = eZDB::instance();
         $db->begin();
         $removeVersion->removeThis();
-        $version = $object->createNewVersionIn( $toLanguageCode, $fromLanguageCode, false, true, eZContentObjectVersion::STATUS_INTERNAL_DRAFT );
+        $version = $object->createNewVersionIn( $toLanguageCode, $fromLanguageCode, false, true, eZContentObjectVersion::STATUS_DRAFT );
         $db->commit();
-
+        unLock( $object );
         return $Module->redirect( 'content', 'edit', array( $objectId, $version->attribute( 'version' ), $toLanguageCode, $fromLanguageCode ) );
-
-        return eZModule::HOOK_STATUS_CANCEL_RUN;
     }
     else
     {
         $http->setSessionVariable( 'ExcessVersionHistoryLimit', true );
         $currentVersion = $object->attribute( 'current_version' );
+        unLock( $object );
         return $Module->redirect( 'content', 'history', array( $objectId, $currentVersion, $toLanguageCode ) );
-        return eZModule::HOOK_STATUS_CANCEL_RUN;
+    }
+}
+
+function lock( eZContentObject $object )
+{
+    while ( true )
+    {
+        if ( isLocked( $object ) )
+            sleep( 1 );
+        else
+            break;
+    }
+    $rowPending = array(
+        'action' => 'creating_translation',
+        'param' => $object->attribute( 'id' )
+    );
+    $pendingItem = new eZPendingActions( $rowPending );
+    $pendingItem->store();
+}
+
+function isLocked( eZContentObject $object )
+{
+    $filterConds = array(
+        'action' => 'creating_translation',
+        'param' => $object->attribute( 'id' )
+    );
+    return count( eZPersistentObject::fetchObjectList( eZPendingActions::definition(), null, $filterConds ) ) > 0;
+}
+
+function unLock( eZContentObject $object )
+{
+    $filterConds = array(
+        'action' => 'creating_translation',
+        'param' => $object->attribute( 'id' )
+    );
+    $rows = eZPersistentObject::fetchObjectList( eZPendingActions::definition(), null, $filterConds );
+    foreach( $rows as $row )
+    {
+        $row->remove();
     }
 }
