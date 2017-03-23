4e09b214110d58a2e54bac3dbfc2c4bb0e8a3ac9
Luca Realdi <lr@opencontent.it>
2017-03-23 10:53:40 +0100

    fix selection attribute-edit

--- classes/ocbserverfunctions.php
+++ classes/ocbserverfunctions.php
@@ -16,6 +16,25 @@ class ocbServerFunctions
             $attribute = eZContentObjectAttribute::fetch( $attributeId, $version );
             if ( $attribute instanceof eZContentObjectAttribute && $attribute->attribute( 'contentobject_id' ) == $objectId )
             {
+                if ($attribute->attribute('data_type_string') == eZSelectionType::DATA_TYPE_STRING){
+                    $classContent = $attribute->contentClassAttribute()->content();
+                    $convertedContent = array();
+                    $optionArray = $classContent['options'];
+                    if (!is_array($content)){
+                        $content = array($content);
+                    }
+                    foreach ( $content as $id )
+                    {
+                        foreach ( $optionArray as $option )
+                        {
+                            $optionId = $option['id'];
+                            if ( $optionId == $id )
+                                $convertedContent[] = $option['name'];
+                        }
+                    }
+                    $content = implode('|', $convertedContent);
+                }
+
                 $params['attributes'] = array( $attribute->attribute( 'contentclass_attribute_identifier' ) => $content );
                 eZContentFunctions::updateAndPublishObject( $object, $params );
                 $object = eZContentObject::fetch( $objectId );
